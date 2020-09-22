dc3aa272fcb93d8040c9f335619d116b093bdc4d
maria antomil <antomilcollar@gmail.com>
2020-09-22 20:39:49 +0200

    updated

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const authenticateJWT = require('../middlewares/autentication');
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const Usuarios = require('../models/Usuarios');
 
 
@@ -20,6 +21,8 @@ router.get('/', authenticateJWT, (req, res) => {
 
 
 router.post('/', (req, res) => {
+
+	
 	const nombreCompleto = req.body.nombreCompleto
 	const foto = req.body.foto
 	const email = req.body.email
@@ -27,57 +30,86 @@ router.post('/', (req, res) => {
 	const contact = req.body.contact
 	const alimentos = req.body.alimentos
 
-	const usuario = new Usuarios()
+	bcrypt.hash(password, saltRounds, function (err, hash) {
+		const usuario = new Usuarios()
 
-	usuario.nombreCompleto = nombreCompleto;
-	usuario.foto = foto;
-	usuario.email = email;
-	usuario.password = password;
-	usuario.contact = contact;
-	usuario.alimentos = alimentos;
+		usuario.nombreCompleto = nombreCompleto;
+		usuario.foto = foto;
+		usuario.email = email;
+		usuario.password = hash;
+		usuario.contact = contact;
+		usuario.alimentos = alimentos;
 
 
-	usuario.save()
-		.then((newUsuario) => {
-			res.json(newUsuario);
-		})
-		.catch((error) => {
-			res.send(error);
-		})
-
+		usuario.save()
+			.then((newUsuario) => {
+				res.json(newUsuario);
+			})
+			.catch((error) => {
+				res.send(error);
+			})
+	});
 });
 
-router.post('/login',  (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+/*router.post('/login', (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
 
-    Usuarios.findOne({ email: email })
-    .then((usuario) => {
-        if(usuario)
-        {
-            bcrypt.compare(password, usuario.password, function(err, result) {
-                if(result)
-                {
-                    const accessToken = jwt.sign(
-                        { userID: user._id, nombreCompleto: usuario.nombreCompleto },
-                        process.env.JWT_SECRET);
-                    return res.json({ logged : true, token: accessToken})
-                }
-                else
-                {
-                    return res.status(404).json({ logged : false})
-                }
-            });
-        }
-        else
-        {
-            return res.status(404).json({ logged : false})
-        }
-    })
-    .catch((err)=>{
-        return res.status(404).json({ logged : false})
-    })
-});
+	Usuarios.findOne({ email: email })
+		.then((usuario) => {
+			if (usuario) {
+				bcrypt.compare(password, usuario.password, function (err, result) {
+					if (result) {
+						const accessToken = jwt.sign(
+							{ userID: user._id, nombreCompleto: usuario.nombreCompleto },
+							process.env.JWT_SECRET);
+						return res.json({ logged: true, token: accessToken })
+					}
+					else {
+						return res.status(404).json({ logged: false })
+					}
+				});
+			}
+			else {
+				return res.status(404).json({ logged: false })
+			}
+		})
+		.catch((err) => {
+			return res.status(404).json({ logged: false })
+		})
+	});*/
+
+	router.post('/login',  (req, res) => {
+		const email = req.body.email;
+		const password = req.body.password;
+		Usuarios.findOne({ email : email })
+			.then((usuario) => {
+				if(usuario)
+				{
+					bcrypt.compare(password, usuario.password, function(err, result) {
+						if(result)
+						{
+							const accessToken = jwt.sign(
+								{ userID: usuario._id, nombreCompleto: usuario.nombreCompleto },
+								process.env.JWT_SECRET);
+							return res.json({ logged : true, token: accessToken})
+						}
+						else
+						{
+							return res.status(404).json({ logged : false})
+						}
+					});
+				}
+				else
+				{
+					return res.status(404).json({ logged : false})
+				}
+			})
+			.catch((err)=>{
+				return res.status(404).json({ logged : false})
+			})
+	});
+
 
 
 
@@ -97,12 +129,12 @@ router.delete('/:id', (req, res) => {
 	const id = req.params.id
 
 	Usuarios.findByIdAndDelete(id)
-	.then((deletedUsuario) => {
-		res.send({mensaje: `se ha borrado correctamente el usuario con id ${id}`});
-	})
-	.catch((error)=>{
-		res.status(500).send(error);
-	})
+		.then((deletedUsuario) => {
+			res.send({ mensaje: `se ha borrado correctamente el usuario con id ${id}` });
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		})
 });
 
 module.exports = router;
